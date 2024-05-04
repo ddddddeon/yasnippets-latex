1af378481fb677a2419ee27d6fd0d62e3f92188b
ddddddeon <opfunk@gmail.com>
2024-05-04 13:39:22 -0400

    Update install script and tweak sum and integral snippets

--- install.sh
+++ install.sh
@@ -2,20 +2,22 @@
 
 # Check for command line args.
 if [ "$1" = "" ]; then
-	echo "Usage: ./install.sh yasnippet-dir"
-	echo "Example: ./install.sh ~/.emacs.d/plugins/yasnippet"
-	exit 1
+    EMACS_DIR="$HOME/.emacs.d"
+else
+    EMACS_DIR="$1"
 fi
 
-# Create the latex-mode dir.
-if [ ! -d "$1"/snippets/latex-mode ]; then
-	mkdir "$1"/snippets/latex-mode
-fi
 
-# Copy needed files to the latex-mode dir.
-cp snippets/*.yasnippet "$1"/snippets/latex-mode/
-cp snippets/.yas-parents "$1"/snippets/latex-mode/
-cp snippets/.yas-ignore-filenames-as-triggers "$1"/snippets/latex-mode/
-cp snippets/.yas-make-groups "$1"/snippets/latex-mode/
+MODES="org-mode latex-mode"
 
-exit 0
+for MODE in $MODES; do
+    DIR="$EMACS_DIR/snippets/$MODE"
+    if [ ! -d "$DIR" ]; then
+	    mkdir "$DIR"
+    fi
+
+    find snippets/ -type f | while read file; do
+        ABSPATH=$(readlink -f "$file")
+        ln -s "$ABSPATH" "$DIR/"
+    done
+done
